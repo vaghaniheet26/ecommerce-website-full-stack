29e583c5cef738470b201e2cd71ae1938fa18143
vaghaniheet26 <117271882+vaghaniheet26@users.noreply.github.com>
2024-01-29 23:02:21 +0530

    Delete E-commerce directory

--- E-commerce/src/pages/Favorite.jsx
+++
@@ -1,8 +0,0 @@
-import FavoriteComponent from '../components/favorite/Favorite';
-const Favorite = () => {
-  return (
-    <FavoriteComponent category="favorite"></FavoriteComponent>
-  )
-}
-
-export default Favorite
